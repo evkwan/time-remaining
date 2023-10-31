8a2f20b873ff03332b4f98d28ff8744b0aacb8fa
Ei-Vonne Kwan <evkwan@gmail.com>
2023-11-01 01:46:40 +0800

    modify timer display

--- src/app/components/Timer.tsx
+++ src/app/components/Timer.tsx
@@ -19,29 +19,50 @@ const Timer = () => {
   if (now === null) return <div>Loading...</div>;
 
   const endOfYear = dayjs().endOf('year');
+  const timeDiff = endOfYear.diff(now);
+
   const milliseconds = dayjs
-    .duration(endOfYear.diff(now))
+    .duration(timeDiff)
     .milliseconds()
     .toString()
     .padStart(2, '0');
 
   const seconds = dayjs
-    .duration(endOfYear.diff(now))
+    .duration(timeDiff)
     .seconds()
     .toString()
     .padStart(2, '0');
 
   const minutes = dayjs
-    .duration(endOfYear.diff(now))
-    .asMinutes()
+    .duration(timeDiff)
+    .minutes()
     .toString()
-    .split('.')[0];
+    .padStart(2, '0');
+
+  const hours = dayjs.duration(timeDiff).asHours().toString().split('.')[0];
 
   return (
-    <div className="w-full flex justify-center">
-      <h2 className="text-center">
-        {`${minutes}  :  ${seconds}  .  ${milliseconds.substring(0, 2)}`}
-      </h2>
+    <div className="w-3/5 flex justify-between items-end bg-slate-300 min-w-fit p-12 rounded-md">
+      <div className="flex flex-col justify-center items-center">
+        <div>{`hours`}</div>
+        <div className="text-6xl">{`${hours}`}</div>
+      </div>
+
+      <div className="text-6xl">{`:`}</div>
+
+      <div className="flex flex-col items-center">
+        <div>{`mins`}</div>
+        <div className="text-6xl">{`${minutes}`}</div>
+      </div>
+
+      <div className="text-6xl">{`:`}</div>
+
+      <div className="flex flex-col items-start w-40 pe-6 ">
+        <div className="text-center">{`seconds`}</div>
+        <div className="text-6xl">
+          {`${seconds}.${milliseconds.substring(0, 2)}`}
+        </div>
+      </div>
     </div>
   );
 };
